83a65f4f50d7db03a83ea6365469059da4125378
svidgen <iambipedal@gmail.com>
2016-07-16 13:58:40 -0500

    (re?)adding files that appear to belong in the solution

---
+++ QuickDAL/DebugSQL.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace QuickDAL
+{
+    public static class DebugSQL
+    {
+        public static string GetActualQuery(IDbCommand sqlCmd)
+        {
+            string query = sqlCmd.CommandText;
+            string parameters = "";
+            string[] strArray = System.Text.RegularExpressions.Regex.Split(query, " VALUES ");
+
+            parameters = "(";
+
+            int count = 0;
+            foreach (SqlParameter p in sqlCmd.Parameters)
+            {
+                if (count == (sqlCmd.Parameters.Count - 1))
+                {
+                    parameters += p.Value.ToString();
+                }
+                else
+                {
+                    parameters += p.Value.ToString() + ", ";
+                }
+                count++;
+            }
+
+            parameters += ")";
+
+            return strArray[0] + " VALUES " + parameters;
+        }
+    }
+}
---
+++ QuickDAL/ICacheCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickDAL
+{
+    public interface ICacheCollection
+    {
+        Object this[String key]  {  get; set;  }
+
+        void Clear();
+
+    }
+}
