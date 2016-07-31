e2e5a2063f0d78d96d29d2235dd1907a56cf5c8a
svidgen <iambipedal@gmail.com>
2016-07-31 16:16:06 -0500

    added projects for sample web app

---
+++ SampleAppServices/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAppServices
+{
+    public class Class1
+    {
+    }
+}
